5c0a1e3a57fc4d249a0b015c002db1d7ce3354c0
Jerry7379 <407847057@qq.com>
2022-08-25 20:34:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 from datetime import date, datetime
+from zhdate import ZhDate as lunar_date
 import math
 from wechatpy import WeChatClient
 from wechatpy.client.api import WeChatMessage, WeChatTemplate
 import requests
-import os
 import random
 
 today = datetime.now()
-start_date = os.environ['START_DATE']
-city = os.environ['CITY']
-birthday = os.environ['BIRTHDAY']
+start_date = '2014-10-23'
+city = '北京'
+birthday = '04-16'
 
-app_id = os.environ["APP_ID"]
-app_secret = os.environ["APP_SECRET"]
+app_id = 'wx4ae406e43a509399'
+app_secret = 'c5a27f0ac787b86d00b8e95a90623282'
 
-user_id = os.environ["USER_ID"]
-template_id = os.environ["TEMPLATE_ID"]
+user_id = 'oN8p-6XF9LPmHNSMbAVtuIrSU-ls'
+# user_id = 'oN8p-6eKgr2ORDzUAV1L4J3M5S6E'
+template_id = 'brXJ2SQ91WvwE3G9ihVpNE58OTVHO_YjEt2QGr5htmw'
 
 
 def get_weather():
@@ -29,7 +30,9 @@ def get_count():
   return delta.days
 
 def get_birthday():
-  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
+  # next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
+  # print(lunar_date(str(date.today().year), 4, 16))
+  next = lunar_date(date.today().year, 4, 16).to_datetime()
   if next < datetime.now():
     next = next.replace(year=next.year + 1)
   return (next - today).days
